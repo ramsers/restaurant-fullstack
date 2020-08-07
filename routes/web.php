a1dccd7d25cb720c8513c9968619a4536adef0d0
Raheim <raheimbbailey@gmail.com>
2020-08-06 22:35:32 -0400

    add users views, controllers, & routes

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,13 @@ Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers'
 
 Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 
+// Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+
+Route::get('/admin/users/create', 'admin\UsersController@create');
+
+Route::get('/admin/users/edit', 'admin\UsersController@edit');
+
 // Route::get('/admin/food-items/{id}/edit','admin\FoodItemsController@edit' );
 
 // Admin Authentication
